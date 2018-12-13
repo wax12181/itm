397f0062a6eff95ab21fbb7e6e5bcd7933608680
wax12181 <wuanxiang@outlook.com>
2018-12-14 00:14:56 +0800

    build http client

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-from urllib2 import Request, urlopen
-from base64 import encodestring
-import json
+from HttpClient import Client
 
 url = 'http://dev.snc.360es.cn:8181/restconf/operations/snc-device:get-device'
 account = 'admin'
@@ -10,17 +8,6 @@ passwd = 'admin'
 
 data = {'input': {}}
 
-req = Request(url, json.dumps(data))
+client = Client(account, passwd, url=url)
 
-b64str = encodestring('%s:%s' % (account, passwd))[:-1]
-
-req.add_header('Authorization', 'Basic %s' % b64str)
-req.add_header('Content-type', 'application/json')
-
-res = urlopen(req)
-
-result = res.readline()
-
-print json.dumps(json.loads(result), indent=4)
-
-res.close()
+print client.post(data)
