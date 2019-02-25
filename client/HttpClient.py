554ed969505cd1866108855561461481ffd16b37
wuanxiang <wuanxiang@360.net>
2019-02-25 14:33:37 +0800

    :sparkles: feat(1.0): 适配cookie
    Signed-off-by: wuanxiang <wuanxiang@360.net>

--- client/HttpClient.py
+++ client/HttpClient.py
@@ -10,13 +10,20 @@ class Client:
 
     __account = 'admin'
     __password = 'admin'
+    __contentType = 'application/json'
+    __cookie_expected = None
+    __captcha = None
     __url = None
 
-    def __init__(self, account=None, password=None, url=None):
+    def __init__(self, account=None, password=None, contentType=None, captcha=None, url=None):
         if account is not None:
             self.__account = account
         if password is not None:
             self.__password = password
+        if contentType is not None:
+            self.__contentType = contentType
+        if captcha is not None:
+            self.__captcha = captcha
         if url is not None:
             self.__url = url
 
@@ -29,17 +36,31 @@ class Client:
     def setPassword(self, password):
         self.__password = password
 
+    def setContentType(self, contentType):
+        self.__contentType = contentType
+
+    def setCaptcha(self, captcha):
+        self.__captcha = captcha
+
     def post(self, data=None):
         if data is None:
             data = {}
         result = self.__open(data)
         if result is not None and result is not '':
-            return json.loads(result)
-    
+            try:
+                return json.loads(result)
+            except ValueError as err:
+                print('error: ' + str(err))
+                return result
+
     def get(self, data=None):
         result = self.__open(data)
         if result is not None:
-            return json.loads(result)
+            try:
+                return json.loads(result)
+            except ValueError as err:
+                print('error: ' + str(err))
+                return result
 
     def put(self, data=None):
         if data is None:
@@ -54,8 +75,10 @@ class Client:
             return
         request = Request(self.__url)
         b64str = encodestring('%s:%s' % (self.__account, self.__password))[:-1]
-        request.add_header('Authorization', 'Basic %s' % b64str)
-        request.add_header('Content-type', 'application/json')
+        request.add_header('Authorization', 'Basic {}'.format(b64str))
+        request.add_header('Content-type', self.__contentType)
+        if self.__captcha is not None:
+            request.add_header('captcha', self.__captcha)
         return request
 
     def __open(self, data=None, method=None):
@@ -69,7 +92,7 @@ class Client:
             res = urlopen(request)
             return res.readline()
         except (HTTPError, URLError) as err:
-            print 'error: ' + str(err)
+            print('error: ' + str(err))
         finally:
             if res is not None:
                 res.close()
